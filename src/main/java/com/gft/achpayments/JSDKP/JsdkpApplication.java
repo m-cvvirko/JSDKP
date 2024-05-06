49af24a0a7ffd7c16c4505fc54ecdebf678053aa
m-cvvirko <marek@cvvirko.pl>
2024-05-06 11:42:50 +0200

    init project

---
+++ src/main/java/com/gft/achpayments/JSDKP/JsdkpApplication.java
@@ -0,0 +1,13 @@
+package com.gft.achpayments.JSDKP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsdkpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsdkpApplication.class, args);
+	}
+
+}
